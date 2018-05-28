fb1fa553c8d9a7147418cbb1668b8fb73e1207a9
Steven La <stevenla@g.ucla.edu>
2018-05-27 19:31:15 -0700

    finished group summary

--- lab3a.c
+++ lab3a.c
@@ -1,3 +1,9 @@
+/*
+NAME: Steven La, Bryan Wong
+EMAIL: stevenla@g.ucla.edu, bryanw0ng@g.ucla.edu
+ID: 004781046, 504744476
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -6,7 +12,6 @@
 #include <errno.h>
 #include <time.h>
 #include <string.h>
-
 #include "ext2_fs.h"
 
 struct ext2_super_block superblock;
@@ -38,25 +43,51 @@ void scan_superblock(int fd){
 }
 
 void scan_groups(int fd){
-	int bytes_read = pread(fd, &groups_data, sizeof(struct ext2_group_desc), 1024 + sizeof(struct ext2_group_desc));
+	int num_groups = 1 + (superblock.s_blocks_count-1) / superblock.s_blocks_per_group;
+	struct ext2_group_desc group_descripts[num_groups*sizeof(struct ext2_group_desc)];
+	int bytes_read = pread(fd, group_descripts, num_groups*sizeof(struct ext2_group_desc), 1024+sizeof(struct ext2_super_block));
 	if(bytes_read < 0){
 		fprintf(stderr, "Error reading groups: %s\n", strerror(errno));
 		exit(2);
 	}
-	int num_groups = 1 + superblock.s_blocks_count / superblock.s_blocks_per_group;
-	int i=0;
+	int i = 0;
+	int o_inodecount = superblock.s_inodes_count;
+	int o_inodegroup = superblock.s_inodes_per_group;
+	int o_blockcount = superblock.s_blocks_count;
+	int o_blockgroup = superblock.s_blocks_per_group;
 	while(i < num_groups){
-		
+		if (o_blockcount < superblock.s_blocks_per_group)
+			o_blockgroup = o_blockcount;
+		if (o_inodecount < superblock.s_inodes_per_group)
+			o_inodegroup = o_inodecount;
+		fprintf(stdout, "GROUP,%u,%u,%u,%u,%u,%u,%u,%u\n",
+			i,
+			o_blockgroup,
+			o_inodegroup,
+			group_descripts[i].bg_free_blocks_count,
+			group_descripts[i].bg_free_inodes_count,
+			group_descripts[i].bg_block_bitmap,
+			group_descripts[i].bg_inode_bitmap,
+			group_descripts[i].bg_inode_table
+			);
+		o_inodecount -= superblock.s_inodes_per_group;
+		o_blockcount -= superblock.s_blocks_per_group;
+		i++;
 	}
 }
 
 int main(int argc, char** argv){
+	// option parsing
 	if(argc != 2){
-		fprintf(stderr, "Error: invalid number of parameters given.\n");
+		fprintf(stderr, "Error: invalid number of arguments.\n");
 		exit(1);
 	}
-	char* file_system = argv[1];
-	int fd = open(file_system, O_RDONLY);
+	int fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Error: could not open file system.\n");
+		exit(1);
+	}
+	// produce CSV summaries
 	scan_superblock(fd);
 	scan_groups(fd);
 
